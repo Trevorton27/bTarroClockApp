2dc5108a2b139095e015a908b9d80e6184db773e
Bruce Tarro <brucetarro@gmail.com>
2020-12-16 07:44:57 -0700

    CSS work; 12/24 button functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Clock App
+// SDMM Clock App
 
 
 var myRefresh = setInterval(myClock, 1000);
@@ -11,14 +11,14 @@ function myClock() {
         'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th',
         'th', 'th', 'st']
 
-    var d = new Date()
+    let d = new Date()
 
-    var currentTime = d.toLocaleTimeString();
-    var currentDate = days[d.getDay()] + ',' + ' ' + months[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
+    let currentTime = d.toLocaleTimeString();
+    let currentDate = days[d.getDay()] + ',' + ' ' + months[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
 
-    var numSuffix = currentDate.toString()
 
     document.getElementById("clock").innerHTML = currentTime;
     document.getElementById("date").innerHTML = currentDate;
+    document.getElementById("clocktype").innerHTML = "12/24h"
 
 }
