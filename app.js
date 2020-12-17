ee9a736dcb9377080db200ab8158c32d15d61b86
Bruce Tarro <brucetarro@gmail.com>
2020-12-16 18:32:27 -0700

    Added 12/24 functionality

--- app.js
+++ app.js
@@ -1,24 +1,77 @@
 // SDMM Clock App
+// Preloads
+
+let myDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let myMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'];
+let suffixes = ['st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th',
+    'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th',
+    'th', 'th', 'st'];
 
 
-var myRefresh = setInterval(myClock, 1000);
+// 12h Local Clock 
 
-function myClock() {
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-        'September', 'October', 'November', 'December'];
-    let suffixes = ['st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th',
-        'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th',
-        'th', 'th', 'st']
-
-    let d = new Date()
+// preload loal clock to avoid delay inital display of clock due to interval refresh
+function earlyLocalClock() {
+    let d = new Date();
 
     let currentTime = d.toLocaleTimeString();
-    let currentDate = days[d.getDay()] + ',' + ' ' + months[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
-
+    let currentDate = myDays[d.getDay()] + ',' + ' ' + myMonths[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
 
     document.getElementById("clock").innerHTML = currentTime;
     document.getElementById("date").innerHTML = currentDate;
-    document.getElementById("clocktype").innerHTML = "12/24h"
+    document.getElementById("clocktype").innerHTML = "24h"
+    localClock();
+}
+
+// 12h Logic & Display
+function localClock() {
+    var newClock = setInterval(myLocalClock, 1000);
+    function myLocalClock() {
+        let d = new Date();
+
+        let currentTime = d.toLocaleTimeString();
+        let currentDate = myDays[d.getDay()] + ',' + ' ' + myMonths[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
+
+        document.getElementById("clock").innerHTML = currentTime;
+        document.getElementById("date").innerHTML = currentDate;
+        document.getElementById("clocktype").innerHTML = "24h"
+        document.getElementById("clocktype").onclick = function () { clearInterval(newClock); earlyMilClock(); };
+
+    }
 
 }
+
+// 24h Military Time Clock
+
+// preload the mil clock to avoid delay inital display of clock due to interval refresh
+function earlyMilClock() {
+    let d = new Date();
+
+    let currentTime = d.toTimeString().slice(0, 8);
+    let currentDate = myDays[d.getDay()] + ',' + ' ' + myMonths[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
+
+    document.getElementById("clock").innerHTML = currentTime;
+    document.getElementById("date").innerHTML = currentDate;
+    document.getElementById("clocktype").innerHTML = "12h"
+    milClock();
+}
+
+// 24h logic & Display
+function milClock() {
+    var newClock = setInterval(myMilClock, 1000);
+    function myMilClock() {
+        let d = new Date();
+
+        let currentTime = d.toTimeString().slice(0, 8);
+        let currentDate = myDays[d.getDay()] + ',' + ' ' + myMonths[d.getMonth()] + ' ' + d.getDate() + suffixes[d.getDate()];
+
+        document.getElementById("clock").innerHTML = currentTime;
+        document.getElementById("date").innerHTML = currentDate;
+        document.getElementById("clocktype").innerHTML = "12h"
+        document.getElementById("clocktype").onclick = function () { clearInterval(newClock); earlyLocalClock(); };
+
+    }
+
+}
+
